7d6b91b1738ab19cdb6a21e1fc415d1a28dff6fc
Enrico Zini <enrico@enricozini.org>
2014-01-13 16:25:06 +0000

    Copyright updated
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/wreport/trunk@4216 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- wreport/var-lua.cc
+++ wreport/var-lua.cc
@@ -1,7 +1,7 @@
 /*
  * wreport/var-lua - Lua bindings to wreport variables
  *
- * Copyright (C) 2010  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ * Copyright (C) 2010--2014  ARPA-SIM <urpsim@smr.arpa.emr.it>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
