fc85773367a93391b93f4b569ed078ef80a01b4f
Emanuele Di Giacomo <edigiacomo@arpae.it>
2017-10-02 14:48:47 +0200

    Use spec in rpmbuild

--- .travis-build.sh
+++ .travis-build.sh
@@ -28,8 +28,9 @@ if [[ $image =~ ^fedora: || $image =~ ^centos: ]]
 then
     pkgname="$(rpmspec -q --qf="wreport-%{version}-%{release}\n" fedora/SPECS/wreport.spec | head -n1)"
     mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+    cp fedora/SPECS/wreport.spec ~/rpmbuild/SPECS/wreport.spec
     git archive --prefix=$pkgname/ --format=tar HEAD | gzip -c > ~/rpmbuild/SOURCES/$pkgname.tar.gz
-    rpmbuild -ba fedora/SPECS/wreport.spec
+    rpmbuild -ba ~/rpmbuild/SPECS/wreport.spec
     find ~/rpmbuild/{RPMS,SRPMS}/ -name "${pkgname}*rpm" -exec cp -v {} . \;
     # TODO upload ${pkgname}*.rpm to github release on deploy stage
 else
